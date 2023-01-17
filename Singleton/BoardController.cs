b9ab3333780aa46502b8095a4a35cbcf96846272
jim <dmt.kokkinos@gmail.com>
2023-01-17 19:18:29 +0200

    singleton

---
+++ Singleton/BoardController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Singleton
+{
+    public class BoardController
+    {
+        private static BoardController instance;
+
+        [MethodImpl(MethodImplOptions.Synchronized)]
+        public static BoardController GetInstance()
+        {
+            if (instance == null)
+                instance = new BoardController();
+            return instance;
+        }
+
+        private bool[] _digitalPins;
+        private int[] _analogPins;
+
+        private BoardController()
+        {
+            _digitalPins = new bool[5];
+            _analogPins = new int[5];
+        }
+
+        public void DigitalWrite(int pin, bool value)
+            => _digitalPins[pin] = value;
+        
+        public void AnalogWrite(int pin, int value)
+            => _analogPins[pin] = value;
+
+        public void PrintState()
+        {
+            Console.WriteLine("digital pins");
+            for (int i = 0; i < _digitalPins.Length; i++)
+            {
+                Console.Write($"pin{i}:{(_digitalPins[i] ? "HIGH" : "LOW" )} ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("analog pins");
+            for (int i = 0; i < _analogPins.Length; i++)
+            {
+                Console.Write($"pin{i}:{(_analogPins[i])} ");
+            }
+        }
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Singleton
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            BoardController controller = BoardController.GetInstance();
+            controller.AnalogWrite(3, 100);
+            controller.DigitalWrite(1, true);
+            controller.PrintState();
+
+            BoardController controller2 = BoardController.GetInstance();
+            controller2.PrintState();
+        }
+    }
+}
