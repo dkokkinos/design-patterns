860a20828a81c675155e531e9b641b14a5616163
dracodean-laptop <dmt.kokkinos@gmail.com>
2023-04-29 18:15:36 +0300

    using dependency injection

---
+++ Composite/ComponentFactory.cs
@@ -0,0 +1,26 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite
+{
+    public class ComponentFactory
+    {
+        private readonly ILifetimeScope _lifeTimeScope;
+
+        public ComponentFactory(ILifetimeScope lifeTimeScope)
+        {
+            _lifeTimeScope = lifeTimeScope;
+        }
+
+        public IComponent Create(string key, object _params)
+        {
+            var props = _params.GetType().GetProperties();
+            var namedParameters = props.Select(x => new NamedParameter(x.Name, x.GetValue(_params, null)));
+            return _lifeTimeScope.ResolveKeyed<IComponent>(key, namedParameters);
+        }
+    }
+}
--- Composite/Program.cs
+++ Composite/Program.cs
@@ -1,17 +1,36 @@
-﻿using Composite.FileSystem;
+﻿using Autofac;
+using Composite.FileSystem;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Composite
 {
     public class Program
     {
+        static IContainer container;
         public static void Main()
         {
-            var root = new Directory("root");
-            var subDir1 = new Directory("subdir1");
-            var subDir2 = new Directory("subdir2");
-            var file1 = new File("file1", new byte[] { 0x33, 0x34 });
-            var file2 = new File("file2", new byte[] { 0x33, 0x34, 0x35 });
+            // Register dependencies.
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<Directory>().Keyed<IComponent>("directory");
+            builder.RegisterType<File>().Keyed<IComponent>("file");
+            builder.RegisterType<ComponentFactory>().AsSelf();
+
+            container = builder.Build();
+
+            RunExample();
+        }
+
+        private static void RunExample()
+        {
+            var componentFactory = container.Resolve<ComponentFactory>();
+            var root = componentFactory.Create("directory", new { name = "root" });
+            var subDir1 = componentFactory.Create("directory", new { name = "subdir1" });
+            var subDir2 = componentFactory.Create("directory", new { name = "subdir2" });
+            var file1 = componentFactory.Create("directory", new { name = "file1", data = new byte[] { 0x33, 0x34 } });
+            var file2 = componentFactory.Create("directory", new { name = "file2", data = new byte[] { 0x33, 0x34, 0x35 } });
 
             root.Add(subDir1);
             root.Add(subDir2);
@@ -32,7 +51,7 @@ namespace Composite
                 Console.WriteLine("Found component: " + searchResult.GetName());
             else
                 Console.WriteLine("Component not found.");
-            
+
             // Remove a file from the file system
             subDir1.Remove(file1);
             Console.WriteLine("Removed file1 from subdir1");
@@ -43,6 +62,13 @@ namespace Composite
             // Get the total size of the file system after removal
             totalSize = root.GetSize();
             Console.WriteLine("Total size of file system after removal: " + totalSize);
+        }
+
+        private static object X(object p)
+        {
+            var props = p.GetType().GetProperties();
+            var pairDictionary = props.ToDictionary(x => x.Name, x => x.GetValue(p, null));
+            return pairDictionary;
         }
     }
 }
