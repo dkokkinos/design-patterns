c55323e6a6b1b07206e3c587cf7bf9861add7f0b
jim <dmt.kokkinos@gmail.com>
2023-05-02 19:19:43 +0300

    add ImageFile in example

--- Composite/Program.cs
+++ Composite/Program.cs
@@ -11,11 +11,15 @@ namespace Composite
             var subDir1 = new Directory("subdir1");
             var subDir2 = new Directory("subdir2");
             var file1 = new File("file1", new byte[] { 0x33, 0x34 });
+            var imageFile = new ImageFile("img1", 800, 600);
+            var imageFile2 = new ImageFile("img2", 800, 600);
             var file2 = new File("file2", new byte[] { 0x33, 0x34, 0x35 });
 
             root.Add(subDir1);
             root.Add(subDir2);
             subDir1.Add(file1);
+            subDir1.Add(imageFile);
+            subDir1.Add(imageFile2);
             subDir2.Add(file2);
 
             // Display the contents of the file system
@@ -32,7 +36,8 @@ namespace Composite
                 Console.WriteLine("Found component: " + searchResult.GetName());
             else
                 Console.WriteLine("Component not found.");
-            
+            searchResult.Add(new Directory("subdir3")); // The searchResult will ingore the Add operaton because its a File.
+
             // Remove a file from the file system
             subDir1.Remove(file1);
             Console.WriteLine("Removed file1 from subdir1");
