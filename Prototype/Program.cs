868b185bf054ce3e94e54e5d1ac3de2958ebcc91
dracodean-laptop <dmt.kokkinos@gmail.com>
2023-06-19 15:25:43 +0300

    prototype

---
+++ Prototype/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    public class Program
+    {
+
+        public static void Main()
+        {
+            Monster prototype = new Monster() { 
+                Name = "Alchemist",
+                Kind = "Agility"
+            };
+
+            var newMonster = prototype.Clone() as Monster;
+            newMonster.Name = "Doom";
+
+
+
+            // Create registry and register prototypes
+            var registry = new ConfigurationRegistry();
+            registry.Register("admin-debug", new Configuration(false, "Admin", "connStr", "apiKey", true));
+            registry.Register("admin-release", new Configuration(false, "Admin", "connStr", "apiKey", false));
+            registry.Register("user-debug", new Configuration(true, "User", "connStr", "apiKey2", true));
+
+            var conf = registry.GetConfiguration("admin-release");
+            conf.SetConnectionString("connStr2");
+
+        }
+
+        // Prototype Registry
+        public class ConfigurationRegistry
+        {
+            private readonly Dictionary<string, Configuration> prototypes = new();
+
+            public Configuration GetConfiguration(string key)
+                => prototypes.FirstOrDefault(x => x.Key == key)?.Value;
+
+            public void Register(string key, Configuration conf)
+                => prototypes.Add(key, conf);
+        }
+
+
+        public class Configuration : ICloneable
+        {
+            public bool IsUserLogedIn { get; }
+            public string Role { get; }
+            public string ConnectionString { get; private set; }
+            public string ApiKey { get; }
+            public bool IsDebug { get; set; }
+
+            public Configuration(bool isUserLogedIn, string role, string connectionString, string apiKey, bool isDebug)
+            {
+                IsUserLogedIn = isUserLogedIn;
+                Role = role;
+                ConnectionString = connectionString;
+                ApiKey = apiKey;
+                IsDebug = isDebug;
+            }
+
+            private Configuration(Configuration source)
+            {
+                IsUserLogedIn = source.IsUserLogedIn;
+                Role = source.Role;
+                ConnectionString = source.ConnectionString;
+                ApiKey = source.ApiKey;
+                IsDebug = source.IsDebug;
+            }
+
+            public void SetConnectionString(string connectionString)
+                => ConnectionString = connectionString;
+
+            public object Clone()
+            {
+                return new Configuration(this);
+            }
+        }
+
+
+
+        public class Graphics
+        {
+            public void Draw(Monster monster, Point position)
+            {
+                // draw at position
+            }
+        }
+
+        public class Monster : ICloneable
+        {
+            public string Name { get; set; }
+            public string Kind { get; set; }
+
+            public Graphics Graphics { get; }
+
+            public Monster()
+            {
+                Graphics = new Graphics(); // time consuming functionality 
+            }
+
+            private Monster(Graphics graphics, string name, string kind)
+            {
+                Graphics = graphics;
+                Name = name;
+                Kind = kind;
+            }
+
+            public void Draw()
+            {
+                Graphics.Draw(this, position: new Point(10, 10));
+            }
+
+            public object Clone()
+            {
+                // we set the already created properties
+                return new Monster(Graphics, Name, Kind);
+            }
+        }
+    }
+}
