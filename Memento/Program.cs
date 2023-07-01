d0d2264f0f0adda531c3201a9ae19fbdb0546299
dracodean-laptop <dmt.kokkinos@gmail.com>
2023-07-01 14:32:16 +0300

    memento

---
+++ Memento/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Memento
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            MinesweeperGame game = new MinesweeperGame(15);
+            game.Play();
+
+            while(true)
+            {
+                string command = Console.ReadLine();
+                int x = Convert.ToInt32( command.Split(',')[0]);
+                int y = Convert.ToInt32(command.Split(',')[1]);
+
+                game.Reveal(x, y);
+            }
+
+            game.Play();
+        }
+    }
+}
